02162d6cb72643ec14ef593278c3278cc034f2d7
Rosana Celine <celine.rosana@gmail.com>
2025-03-27 11:16:44 -0300

    First Steps in Java with Spring Boot 3.4.4

---
+++ 04_FirstStepsInJavawithSpringBoot/src/main/java/RosanaCeline/model/Greeting.java
@@ -0,0 +1,4 @@
+package RosanaCeline.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ 04_FirstStepsInJavawithSpringBoot/src/test/java/RosanaCeline/StartupTests.java
@@ -0,0 +1,13 @@
+package RosanaCeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
